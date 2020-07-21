61b761a7093cf181758ca510021fedfb0dad188d
Du Phan <du.phan@dataiku.com>
2020-07-21 10:26:34 +0200

    fix error when there is col type mismatch between 2 df

--- python-lib/dku_model_data_quality/model_utils.py
+++ python-lib/dku_model_data_quality/model_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import logging
 
 logger = logging.getLogger(__name__)
@@ -34,21 +35,24 @@ def check_differences_between_datasets(df_ref, df_test, columns=None, range_mode
     
     result = dict()
     n_test = float(df_test.shape[0])
-    
-    for col_name in df_test.select_dtypes(include=['number']):
+
+    # we concat the 2 df before checking the type of each column. This avoids situation where col A in ds_ref is object (1a,2,3) but in ds_test is numerical (4,5,6)
+    concat_df = pd.concat([df_ref, df_test])
+
+    for col_name in concat_df.select_dtypes(include=['number']):
         if not col_name in columns:
             continue
-        rmin, rmax = get_bounds(df_ref[col_name], mode=range_mode)
-        logger.info('Checking column {}. Lower bound: {}. Upper bound: {}'.format(col_name, rmin, rmax))
+        lower_bound, upper_bound = get_bounds(df_ref[col_name], mode=range_mode)
+        logger.info('Checking column {}. Lower bound: {}. Upper bound: {}'.format(col_name, lower_bound, upper_bound))
         # We count the ratio of samples in test outside of this range
-        n_invalid = np.sum(np.logical_or(df_test[col_name].values < rmin, df_test[col_name].values > rmax))
+        n_invalid = np.sum(np.logical_or(df_test[col_name].values < lower_bound, df_test[col_name].values > upper_bound))
         result[col_name] = n_invalid / n_test
 
-    for col_name in df_test.select_dtypes(include=['object', 'category']):
+    for col_name in concat_df.select_dtypes(include=['object', 'category']):
         if not col_name in columns:
             continue
         ref_categories = df_ref[col_name].unique()
-        logger.info('Checking column {}. Reference categories:'.format(col_name, ref_categories))
+        logger.info('Checking column {}. Reference categories: {}'.format(col_name, ref_categories))
         result[col_name] = 1 - (np.isin(df_test[col_name].values, ref_categories).sum()) / n_test
     
     return result
--- python-probes/feature_difference/probe.py
+++ python-probes/feature_difference/probe.py
@@ -10,31 +10,6 @@ def process(ds_test, partition_id):
     # named config , and the plugin-level parameters (if any) as 
     # a dict named plugin_config
 
-    """
-    columns = None
-    df_ref = None
-
-    if config.get('input_mode') == 'dataset':
-        df_ref = dataiku.Dataset(config.get("ds_ref")).get_dataframe()
-        columns = config.get("columns_dataset")
-    else:
-        model_ref = config.get('model_ref')
-        model = dataiku.Model(model_ref)
-        model_handler = get_model_handler(model)
-        model_accessor = ModelAccessor(model_handler)
-        df_ref = model_accessor.get_original_test_df()
-        selected_features = model_accessor.get_selected_features()
-        chosen_columns = config.get("columns_model")
-        if len(chosen_columns) > 0:
-            columns = chosen_columns
-            features_not_in_model = list(set(columns) - set(selected_features))
-            if len(features_not_in_model) > 0:
-                raise ValueError('The following chosen columns are not used in the model: {}. Please remove them from the list of columns to check'.format(features_not_in_model))
-        else:
-            columns = selected_features
-
-    range_mode = config.get('range_mode')
-    """
     df_test = ds_test.get_dataframe()
     df_ref, columns, range_mode = get_params(config)
     diff = check_differences_between_datasets(df_ref, df_test, columns=columns, range_mode=range_mode)
