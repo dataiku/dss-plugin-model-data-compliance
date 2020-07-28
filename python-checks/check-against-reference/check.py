738b63d38a8752d22f2f9172db2fc07940ec506a
Du Phan <du.phan@dataiku.com>
2020-07-28 17:30:50 +0200

    python36 compability

--- python-checks/check-against-reference/check.py
+++ python-checks/check-against-reference/check.py
@@ -11,7 +11,7 @@ def process(last_values, ds_test, partition_id):
     diff = check_differences_between_datasets(df_ref, df_test, columns=columns, range_mode=range_mode)
 
     message = ' '.join(['[{}, {:.2f}, {}]'.format(col_name, ratio, 'PASSED' if ratio <= tolerance else 'FAILED') for col_name, ratio in diff.items()])
-    anomaly = np.any(np.asarray(diff.values()) > tolerance)
+    anomaly = np.any(np.asarray(list(diff.values())) > tolerance)
     result = 'OK'
     if anomaly:
         result = 'ERROR' if werror else 'WARNING'
